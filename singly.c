64123e77259fe69579272ec0674794627ac2a6d2
Anandakrishnan <anandakrishnananish1@gmail.com>
2025-11-04 14:38:28 +0530

    Update singly.c

--- singly.c
+++ singly.c
@@ -1,40 +1,37 @@
-#include <stdio.h>
-#include <stdlib.h>
-	struct Node {
-    		int data;
-    		struct Node* next;
-		};
-int main() 
-	{
-    	struct Node* head = NULL;
-    	struct Node* temp = NULL;
-    	struct Node* newNode = NULL;
-    	int n, i, value;
-    	printf("Enter number of nodes: ");
-    	scanf("%d", &n);
-   	for (i = 0; i < n; i++) 
-        	newNode = (struct Node*)malloc(sizeof(struct Node)); 
-        	printf("Enter data for node %d: ", i + 1);
-        	scanf("%d", &value);
-        	newNode->data = value;
-        	newNode->next = NULL;
-        		if (head == NULL) 
-        			{
-            				head = newNode; 
-            				temp = head;
-        			} 
-        		else 
-        			{
-            				temp->next = newNode;
-            				temp = temp->next;
-        			}
-    				printf("\nSingly Linked List: ");
-    				temp = head;
-    				while (temp != NULL) 
-    					{
-        					printf("%d -> ", temp->data);
-        					temp = temp->next;
-    					}
-    				printf("NULL\n");
-    return 0;
+#include<stdio.h>
+#include<stdlib.h>
+struct node {
+int data;
+struct node*next;
+};
+int main(){
+struct node*head=NULL;
+struct node*temp=NULL;
+struct node*newnode=NULL;
+int n,i,value;
+printf("Enter number of nodes:");
+scanf("%d",&n);
+for(i=0;i<n;i++){
+newnode=(struct node*)malloc(sizeof(struct node));
+printf("Enter data for node %d:",i+1);
+scanf("%d",&value);
+newnode->data=value;
+newnode->next=NULL;
+if(head==NULL){
+head=newnode;
+temp=head;
+}else{
+temp->next=newnode;
+temp=temp->next;
 }
+}
+printf("\nSingly linked list:");
+temp=head;
+while(temp !=NULL){
+printf("%d->",temp->data);
+temp=temp->next;
+}
+printf("NULL\n");
+return 0;
+} 
+
